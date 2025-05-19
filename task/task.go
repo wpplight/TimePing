06f0f1dd38a63bbcd37f21992e70029152da4cb2
Nior <15496219+nior111@user.noreply.gitee.com>
2025-05-19 22:58:36 +0800

    待更改

--- task/task.go
+++ task/task.go
@@ -2,6 +2,7 @@ package task
 
 import (
 	"container/list"
+	"time"
 	t "timeping/utype"
 )
 
@@ -9,7 +10,6 @@ func creatUnusedTask(TaskPoolNode []t.TaskNode )  {
 	t.Engine_kernal.UnuseQueue=list.New()
 	for i:=0;i<len(TaskPoolNode);i++{
 		TaskPoolNode[i].Used = false
-		t.Engine_kernal.UnuseQueue.PushBack(&TaskPoolNode[i].Tnode)
+		t.Engine_kernal.Unus  eQueue.PushBack(&TaskPoolNode[i].Tnode)
 	}
 }
-
